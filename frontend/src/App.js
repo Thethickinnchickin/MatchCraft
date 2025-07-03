e5da056a637c3381a47fd2e1764d015c4a870dc8
Matthew Reiley <mattreileydeveloper@gmail.com>
2025-07-03 14:59:08 -0700

    debugging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,9 +37,8 @@ function App() {
 
       const letterData = await letterRes.json();
       setCoverLetter(letterData.cover_letter);
-      if(letterData.length > 0) {
-        setLoading(false);
-      }
+      setLoading(false);
+      
     } finally {
       setLoading(false);
     }
